2cb13b069766e32876aa39f4f6923480410c55d0
MikiKung <mikiofficial002@gmail.com>
2021-12-04 00:22:36 +0700

    for Heroku

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const { Client, Intents } = require('discord.js');
 const dotenv = require("dotenv");
 const schedule = require("./schedule.json")
+const express = require("express")
+
+const app = express()
 
 dotenv.config()
+
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+    console.log("Burger Chan is Ready on Express")
+    client.login(process.env.TOKEN)
+})
+
 
 const client = new Client(
     {
@@ -18,7 +27,7 @@ var day = currentdate.getDay()
 
 
 client.on('ready', () => {
-    console.log("Miki Chan is Ready")
+    console.log("Burger Chan is Ready on Bot")
 })
 
 client.on('messageCreate', msg => {
@@ -27,21 +36,20 @@ client.on('messageCreate', msg => {
         msg.reply(schedule.schedule[day])
     }
     else if (msg.content == "$today") {
-        msg.reply(schedule.schedule[day-1])
+        msg.reply(schedule.schedule[day - 1])
     }
-    else if (msg.content == "$lastday"){
-        msg.reply(schedule.schedule[day-2])
+    else if (msg.content == "$lastday") {
+        msg.reply(schedule.schedule[day - 2])
     }
-    else if (msg.content == "$allday"){
+    else if (msg.content == "$allday") {
         var alltext = ""
         for (let i = 0; i < schedule.schedule.length; i++) {
-            alltext += schedule.schedule[i]+"\n"
+            alltext += schedule.schedule[i] + "\n"
         }
         msg.reply(alltext)
     }
-    else if (msg.content == "$help"){
+    else if (msg.content == "$help") {
         msg.reply("$today บอกตารางเรียนวันนี้ \n $nextday บอกตารางเรียนพรุ่งนี้ \n $lastday บอกตารางเรียนเมื่อวาน \n $allday บอกตารางเรียนทั้งหมด \n $help บอก syntax ทั้งหมด")
     }
 })
 
-client.login(process.env.TOKEN)
